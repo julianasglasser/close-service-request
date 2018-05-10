3f0de7bd3d2afc766173689b347e027581a534d9
Juliana Glasser | Stone <jglasser@stone.com.br>
2018-05-10 15:09:58 -0300

    subindo projeto

---
+++ BaixaOS.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace BaixaMassiva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.botaoCancelar = new System.Windows.Forms.Button();
+            this.botaoColar = new System.Windows.Forms.Button();
+            this.passwordField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.OS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serialInstalado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoEquipamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tecnico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataDeAtendimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resultado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToOrderColumns = true;
+            this.grid.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.grid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OS,
+            this.serialInstalado,
+            this.TipoEquipamento,
+            this.modelo,
+            this.Tecnico,
+            this.DataDeAtendimento,
+            this.Resultado});
+            this.grid.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.grid.Location = new System.Drawing.Point(12, 55);
+            this.grid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(864, 408);
+            this.grid.TabIndex = 0;
+            this.grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellContentClick);
+            // 
+            // botaoCancelar
+            // 
+            this.botaoCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.botaoCancelar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoCancelar.Location = new System.Drawing.Point(913, 409);
+            this.botaoCancelar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.botaoCancelar.Name = "botaoCancelar";
+            this.botaoCancelar.Size = new System.Drawing.Size(132, 29);
+            this.botaoCancelar.TabIndex = 1;
+            this.botaoCancelar.Text = "Baixar OS";
+            this.botaoCancelar.UseVisualStyleBackColor = true;
+            this.botaoCancelar.Click += new System.EventHandler(this.botaoCancelar_Click);
+            // 
+            // botaoColar
+            // 
+            this.botaoColar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.botaoColar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoColar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.botaoColar.Location = new System.Drawing.Point(732, 13);
+            this.botaoColar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.botaoColar.Name = "botaoColar";
+            this.botaoColar.Size = new System.Drawing.Size(144, 30);
+            this.botaoColar.TabIndex = 2;
+            this.botaoColar.Text = "Colar planilha";
+            this.botaoColar.UseVisualStyleBackColor = true;
+            this.botaoColar.Click += new System.EventHandler(this.botaoColar_Click);
+            // 
+            // passwordField
+            // 
+            this.passwordField.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordField.Location = new System.Drawing.Point(913, 334);
+            this.passwordField.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.passwordField.Name = "passwordField";
+            this.passwordField.Size = new System.Drawing.Size(132, 25);
+            this.passwordField.TabIndex = 3;
+            this.passwordField.UseSystemPasswordChar = true;
+            this.passwordField.TextChanged += new System.EventHandler(this.passwordField_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(909, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Senha";
+            // 
+            // userField
+            // 
+            this.userField.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.userField.Location = new System.Drawing.Point(913, 242);
+            this.userField.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.userField.Name = "userField";
+            this.userField.Size = new System.Drawing.Size(132, 25);
+            this.userField.TabIndex = 5;
+            this.userField.TextChanged += new System.EventHandler(this.userField_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(909, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Usuário";
+            // 
+            // restart
+            // 
+            this.restart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.restart.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart.Location = new System.Drawing.Point(913, 13);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(132, 30);
+            this.restart.TabIndex = 7;
+            this.restart.Text = "Reiniciar";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // OS
+            // 
+            this.OS.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.OS.HeaderText = "OS";
+            this.OS.Name = "OS";
+            this.OS.Width = 50;
+            // 
+            // serialInstalado
+            // 
+            this.serialInstalado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.serialInstalado.HeaderText = "Serial Instalado";
+            this.serialInstalado.Name = "serialInstalado";
+            this.serialInstalado.Width = 125;
+            // 
+            // TipoEquipamento
+            // 
+            this.TipoEquipamento.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.TipoEquipamento.HeaderText = "Tipo de Equipamento";
+            this.TipoEquipamento.Name = "TipoEquipamento";
+            this.TipoEquipamento.Width = 149;
+            // 
+            // modelo
+            // 
+            this.modelo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.modelo.HeaderText = "Modelo";
+            this.modelo.Name = "modelo";
+            this.modelo.Width = 79;
+            // 
+            // Tecnico
+            // 
+            this.Tecnico.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Tecnico.HeaderText = "Técnico";
+            this.Tecnico.Name = "Tecnico";
+            this.Tecnico.Width = 77;
+            // 
+            // DataDeAtendimento
+            // 
+            this.DataDeAtendimento.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.DataDeAtendimento.HeaderText = "Data de Atendimento";
+            this.DataDeAtendimento.Name = "DataDeAtendimento";
+            this.DataDeAtendimento.Width = 150;
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Resultado.HeaderText = "Resultado";
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Width = 93;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 476);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordField);
+            this.Controls.Add(this.botaoColar);
+            this.Controls.Add(this.botaoCancelar);
+            this.Controls.Add(this.grid);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Baixa Massiva Hijack";
+            this.TransparencyKey = System.Drawing.Color.DimGray;
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button botaoCancelar;
+        private System.Windows.Forms.Button botaoColar;
+        private System.Windows.Forms.TextBox passwordField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn serialInstalado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoEquipamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tecnico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataDeAtendimento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resultado;
+    }
+}
+
---
+++ BaixaOS.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaixaMassiva
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+#if DEBUG
+            userField.Text = "jglasser";
+            passwordField.Text = "\"Meubebe\"*38";
+#endif
+        }
+        public string baixaOS(int numeroOS, string serialInstalado, string tipo, string modelo, string tecnico, string data)
+        {
+            // Conecta no WKF
+            ProductionLogisticWKF.WKFServiceClient client = new ProductionLogisticWKF.WKFServiceClient();
+
+            // Cria autenticação do WKF
+            ProductionLogisticWKF.Authentication auth = new ProductionLogisticWKF.Authentication();
+            auth.UserName = userField.Text;
+            auth.Password = passwordField.Text;
+
+            // Define parametros da busca
+            ProductionLogisticWKF.BuscaOS parametrosDaBusca = new ProductionLogisticWKF.BuscaOS();
+            parametrosDaBusca.NumeroOS = numeroOS;
+
+            // Consulta OS
+            ProductionLogisticWKF.RetornoConsultaOS consultaOS = client.WSConsultaOS(auth, parametrosDaBusca, 1, 10);
+            ProductionLogisticWKF.OrdemDeServico os = consultaOS.ListOS[0];
+
+            var statusAtual = consultaOS.ListOS[0].Status;
+
+            if (statusAtual == "NOVA")
+            {
+                os.Status = "RECEBIDA";
+                try
+                {
+                    var result = client.WSAtualizaOS(auth, os);
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        
+
+            // Baixa OS
+            os.Status = "BAIXADA";
+            os.DataAtendimento = DateTime.Now;
+            os.TerminalEntra.NumSerie = serialInstalado;
+            os.TerminalEntra.Modelo = modelo;
+            os.TerminalEntra.TipoEquipamento = tipo;
+
+            if (!string.IsNullOrEmpty(data))
+                os.DataAtendimento = DateTime.Parse(data);
+
+            switch (tecnico)
+            {
+                case "AJUSTE DE ESTOQUE":
+                    os.Tecnico.NomeTecnico = "AJUSTE DE ESTOQUE";
+                    os.Tecnico.NumeroDoc = "12188527712";
+                    break;
+                case "TOMBAMENTO":
+                    os.Tecnico.NomeTecnico = "TOMBAMENTO";
+                    os.Tecnico.NumeroDoc = "111111111111";
+                    break;
+                case "COMERCIAL REDIRECT":
+                    os.Tecnico.NomeTecnico = "COMERCIAL REDIRECT";
+                    os.Tecnico.NumeroDoc = "1";
+                    break;
+                default:
+                    break;
+            }
+            try
+            {
+                var result = client.WSAtualizaOS(auth, os);
+
+                // Fecha conexão
+                client.Close();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao baixar a OS " + numeroOS + ". - Erro: " + ex.Message);
+                return ex.Message;
+            }
+        }
+
+        private void botaoCancelar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Botão apertado! Vamos baixar as OSs.");
+            for (int i = 0; i < grid.Rows.Count; i++)
+            {
+                int j = 0;
+                int numeroOS = Convert.ToInt32(grid.Rows[i].Cells[j].Value?.ToString());
+                j++;
+                var serialInstalado = grid.Rows[i].Cells[j].Value?.ToString();
+                j++;
+                var tipo = grid.Rows[i].Cells[j].Value?.ToString();
+                j++;
+                var modelo = grid.Rows[i].Cells[j].Value?.ToString();
+                j++;
+                var tecnico = grid.Rows[i].Cells[j].Value?.ToString();
+                j++;
+                var data = grid.Rows[i].Cells[j].Value?.ToString();
+
+                if (numeroOS != 0)
+                {
+                    var result = baixaOS(numeroOS, serialInstalado, tipo, modelo, tecnico, data);
+                    j++;
+                    grid.Rows[i].Cells[j].Value = result;
+                }
+            }
+            MessageBox.Show("Operação finalizada.");
+        }
+
+        private void userField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void botaoColar_Click(object sender, EventArgs e)
+        {
+            grid.Rows[0].Cells[0].Selected = true;
+            char[] rowSplitter = { '\r', '\n' };
+
+            char[] columnSplitter = { '\t' };
+
+            //get the text from clipboard
+            IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+            string stringInClipboard = (string)dataInClipboard.GetData(DataFormats.Text);
+
+            //split it into lines
+            string[] rowsInClipboard = stringInClipboard.Split(rowSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+            //get the row and column of selected cell in grid
+            int r = grid.SelectedCells[0].RowIndex;
+
+            int c = grid.SelectedCells[0].ColumnIndex;
+
+            //add rows into grid to fit clipboard lines
+            if (grid.Rows.Count < (r + rowsInClipboard.Length))
+
+            {
+                grid.Rows.Add(r + rowsInClipboard.Length - grid.Rows.Count);
+            }
+
+            // loop through the lines, split them into cells and place the values in the corresponding cell.
+            for (int iRow = 0; iRow < rowsInClipboard.Length; iRow++)
+            {
+                //split row into cell values
+                string[] valuesInRow = rowsInClipboard[iRow].Split(columnSplitter);
+
+                //cycle through cell values
+                for (int iCol = 0; iCol < valuesInRow.Length; iCol++)
+                {
+
+                    //assign cell value, only if it within columns of the grid
+                    if (grid.ColumnCount - 1 >= c + iCol)
+                    {
+                        grid.Rows[r + iRow].Cells[c + iCol].Value = valuesInRow[iCol];
+                    }
+                }
+            }
+        }
+    private void passwordField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            this.grid.Rows.Clear();
+        }
+    }
+}
